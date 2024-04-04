902a193bddb9ddbff83468d8522f3c892f73592c
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2024-04-04 16:04:09 +0200

    Displaying middle point

--- src/model/systems/selectionSystem.cpp
+++ src/model/systems/selectionSystem.cpp
@@ -6,6 +6,7 @@
 
 #include <CAD_modeler/model/systems/pointsSystem.hpp>
 #include <CAD_modeler/model/systems/toriSystem.hpp>
+#include <CAD_modeler/model/systems/cameraSystem.hpp>
 
 #include <stdexcept>
 
@@ -15,20 +16,49 @@ void SelectionSystem::RegisterSystem(Coordinator & coordinator)
     coordinator.RegisterSystem<SelectionSystem>();
 
     coordinator.RegisterComponent<Selected>();
+    coordinator.RegisterComponent<Position>();
 
     coordinator.RegisterRequiredComponent<SelectionSystem, Selected>();
+}
+
+
+SelectionSystem::SelectionSystem():
+    shader("../../shaders/vertexShader.vert", "../../shaders/fragmentShader.frag")
+{
+    std::vector<float> vertices = {
+        0.0f, 0.0f, 0.0f
+    };
+
+    std::vector<uint32_t> indices = { 0 };
+
+    pointsMesh.Update(vertices, indices);
+}
+
+
+void SelectionSystem::Init()
+{
+    middlePoint = coordinator->CreateEntity();
+
+    Position pos;
+    coordinator->AddComponent<Position>(middlePoint, pos);
+
+    coordinator->AddComponent<Mesh>(middlePoint, pointsMesh);
 }
 
 
 void SelectionSystem::Select(Entity entity)
 {
     coordinator->AddComponent<Selected>(entity, Selected());
+
+    UpdateMiddlePointPosition();
 }
 
 
 void SelectionSystem::Deselect(Entity entity)
 {
     coordinator->DeleteComponent<Selected>(entity);
+
+    UpdateMiddlePointPosition();
 }
 
 
@@ -60,4 +90,47 @@ void SelectionSystem::SelectFromLine(const Line& line)
         else
             Select(actEntity);
     }
+}
+
+
+void SelectionSystem::RenderMiddlePoint()
+{
+    if (entities.size() < 2)
+        return;
+
+    auto const& cameraSystem = coordinator->GetSystem<CameraSystem>();
+    auto const& selectionSystem = coordinator->GetSystem<SelectionSystem>();
+
+    glm::mat4x4 cameraMtx = cameraSystem->PerspectiveMatrix() * cameraSystem->ViewMatrix();
+
+    auto const& mesh = coordinator->GetComponent<Mesh>(middlePoint);
+    auto const& position = coordinator->GetComponent<Position>(middlePoint);
+
+    shader.use();
+
+    shader.setVec4("color", glm::vec4(1.0f, 1.0f, 0.0f, 1.0f));
+
+    glm::mat4x4 modelMtx = position.TranslationMatrix();
+    shader.setMatrix4("MVP", cameraMtx * modelMtx);
+
+    mesh.Use();
+    glDrawElements(GL_POINTS, mesh.GetElementsCnt(), GL_UNSIGNED_INT, 0);
+}
+
+
+void SelectionSystem::UpdateMiddlePointPosition()
+{
+    if (entities.size() < 2) {
+        return;
+    }
+
+    Position newPos(0.0f);
+
+    for (auto entity: entities) {
+        newPos.vec += coordinator->GetComponent<Position>(entity).vec;
+    }
+
+    newPos.vec /= entities.size();
+
+    coordinator->GetComponent<Position>(middlePoint) = newPos;
 }
