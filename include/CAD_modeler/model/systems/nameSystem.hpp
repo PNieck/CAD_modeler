15003a803bcdb7ae4d469325eedb375d9d44924a
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2024-05-11 15:31:30 +0200

    Make name system header only

--- include/CAD_modeler/model/systems/nameSystem.hpp
+++ include/CAD_modeler/model/systems/nameSystem.hpp
@@ -2,17 +2,23 @@
 
 
 #include <ecs/system.hpp>
+#include <ecs/coordinator.hpp>
 #include "../components/name.hpp"
 
 
 class NameSystem: public System {
 public:
-    static void RegisterSystem(Coordinator& coordinator);
+    static void RegisterSystem(Coordinator& coordinator) {
+        coordinator.RegisterSystem<NameSystem>();
+        coordinator.RegisterRequiredComponent<NameSystem, Name>();
+    }
 
     inline const std::unordered_set<Entity>& EntitiesWithNames() const
         { return entities; }
 
-    const Name& GetName(Entity entity) const;
+    inline const Name& GetName(Entity entity) const
+        { return coordinator->GetComponent<Name>(entity); }
 
-    void SetName(Entity entity, const Name& name) const;
+    inline void SetName(Entity entity, const Name& name) const
+        { coordinator->SetComponent<Name>(entity, name); }
 };
