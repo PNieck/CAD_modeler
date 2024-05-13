832d3c88d7e6d2f30caee617d61e4c148707917a
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2024-05-13 17:06:27 +0200

    Add solving system of linear equations with tridiagonal matrix

---
+++ include/algebra/systemsOfLinearEquasions.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+#include <cassert>
+
+
+namespace alg
+{
+
+    template <typename ScalarType, typename VectorType>
+    std::vector<VectorType> SolveSystemOfLinearEquationsWithTridiagonalMtx(
+        std::vector<ScalarType>& superdiagonalElems,
+        std::vector<ScalarType>& diagonalElems,
+        std::vector<ScalarType>& subdiagonalElems,
+        std::vector<VectorType>& resultsElems
+    ) {
+        assert(superdiagonalElems.size() == diagonalElems.size() - 1);
+        assert(diagonalElems.size() == subdiagonalElems.size() + 1);
+        assert(diagonalElems.size() == resultsElems.size());
+
+        auto size = diagonalElems.size();
+        std::vector<VectorType> result(size);
+
+        for (int i=0; i < size - 1; ++i) {
+            float coeff = subdiagonalElems[i]/diagonalElems[i];
+            diagonalElems[i+1] -= coeff * superdiagonalElems[i];
+            resultsElems[i+1] -= coeff * resultsElems[i];
+        }
+
+        result[size - 1] = resultsElems[size - 1] / diagonalElems[size - 1];
+
+        for (int i = size - 2; i >= 0; --i) {
+            result[i] = (resultsElems[i] - superdiagonalElems[i] * result[i+1]) / diagonalElems[i];
+        }
+
+        return result;
+    }
+
+}
