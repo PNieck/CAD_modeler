66fd40a5282abb5ec3871a8c0c8083ee6426f633
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2024-10-27 11:26:17 +0100

    Add milling paths rendering

--- include/CAD_modeler/model/systems/millingMachineSystem.hpp
+++ include/CAD_modeler/model/systems/millingMachineSystem.hpp
@@ -5,6 +5,7 @@
 #include <algebra/mat4x4.hpp>
 
 #include "../components/mesh.hpp"
+#include "../components/millingMachinePaths.hpp"
 
 
 class MillingMachineSystem: public System {
@@ -13,7 +14,11 @@ public:
 
     void CreateMaterial(int xResolution, int yResolution);
 
+    void AddPaths(MillingMachinePaths&& paths);
+
     void Render(const alg::Mat4x4& view, const alg::Mat4x4& perspective);
+
+
 
 private:
     unsigned int heightmap = 0;
@@ -22,4 +27,7 @@ private:
 
     std::vector<float> GenerateVertices();
     std::vector<uint32_t> GenerateIndices();
+
+    std::vector<float> GeneratePathsVertices(const MillingMachinePaths& paths);
+    std::vector<uint32_t> GeneratePathsIndices(const MillingMachinePaths& paths);
 };
--- src/modeler/model/systems/millingMachineSystem.cpp
+++ src/modeler/model/systems/millingMachineSystem.cpp
@@ -2,6 +2,8 @@
 
 #include <CAD_modeler/model/systems/shaders/shaderRepository.hpp>
 
+#include <CAD_modeler/model/components/millingMachinePaths.hpp>
+
 #include <ecs/coordinator.hpp>
 
 #include <glad/glad.h>
@@ -10,6 +12,12 @@
 void MillingMachineSystem::RegisterSystem(Coordinator &coordinator)
 {
     coordinator.RegisterSystem<MillingMachineSystem>();
+
+    coordinator.RegisterComponent<MillingMachinePaths>();
+    coordinator.RegisterComponent<Mesh>();
+
+    coordinator.RegisterRequiredComponent<MillingMachineSystem, MillingMachinePaths>();
+    coordinator.RegisterRequiredComponent<MillingMachineSystem, Mesh>();
 }
 
 
@@ -41,24 +49,59 @@ void MillingMachineSystem::CreateMaterial(const int xResolution, const int yReso
 }
 
 
+void MillingMachineSystem::AddPaths(MillingMachinePaths &&paths)
+{
+    const Entity pathsEntity = coordinator->CreateEntity();
+
+    Mesh pathsMesh;
+    pathsMesh.Update(
+        GeneratePathsVertices(paths),
+        GeneratePathsIndices(paths)
+    );
+
+    coordinator->AddComponent(pathsEntity, std::move(pathsMesh));
+    coordinator->AddComponent(pathsEntity, std::move(paths));
+}
+
+
 void MillingMachineSystem::Render(const alg::Mat4x4& view, const alg::Mat4x4& perspective)
 {
-    const auto& shader = ShaderRepository::GetInstance().GetMillingShader();
+    const auto& shaderRepo = ShaderRepository::GetInstance();
+    const auto& shader = shaderRepo.GetMillingShader();
 
     shader.Use();
     glBindTexture(GL_TEXTURE_2D, heightmap);
     shader.SetColor(alg::Vec4(1.0f));
 
+    auto cameraMtx = perspective * view;
+
     shader.SetHeightMapLength(2.f);
     shader.SetHeightMapWidth(2.f);
     shader.SetMainHeightmapCorner(alg::Vec3(-1.f, 0.f, -1.f));
-    shader.SetMVP(perspective * view);
-    //shader.SetViewMtx(view);
+    shader.SetMVP(cameraMtx);
     shader.SetProjection00(perspective(0, 0));
 
     material.Use();
     glPatchParameteri(GL_PATCH_VERTICES, 4);
-    glDrawElements(GL_PATCHES, material.GetElementsCnt(), GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_PATCHES, material.GetElementsCnt(), GL_UNSIGNED_INT, nullptr);
+
+    if (entities.empty())
+        return;
+
+    auto const& stdShader = shaderRepo.GetStdShader();
+
+    stdShader.Use();
+
+    // Neon green
+    stdShader.SetColor(alg::Vec4(23.f/255.f, 227.f/255.f, 33.f/255.f, 1.f));
+    shader.SetMVP(cameraMtx);
+
+    for (auto const entity : entities) {
+        auto const& mesh = coordinator->GetComponent<Mesh>(entity);
+        mesh.Use();
+
+        glDrawElements(GL_LINE_STRIP, mesh.GetElementsCnt(), GL_UNSIGNED_INT, nullptr);
+    }
 }
 
 
@@ -79,3 +122,34 @@ std::vector<uint32_t> MillingMachineSystem::GenerateIndices()
         0, 1, 2, 3
     };
 }
+
+
+std::vector<float> MillingMachineSystem::GeneratePathsVertices(const MillingMachinePaths &paths)
+{
+    std::vector<float> result;
+    result.reserve(paths.GetSize() * alg::Vec3::dim);
+
+    for (const auto& pos : paths.GetPositions()) {
+        result.push_back(pos.GetX());
+        result.push_back(pos.GetY());
+        result.push_back(pos.GetZ());
+    }
+
+    return result;
+}
+
+
+std::vector<uint32_t> MillingMachineSystem::GeneratePathsIndices(const MillingMachinePaths &paths)
+{
+    std::vector<std::uint32_t> result;
+    result.reserve(paths.GetSize());
+
+    for (uint32_t i = 0; i < paths.GetSize(); ++i) {
+        result.push_back(i);
+    }
+
+    return result;
+}
+
+
+
