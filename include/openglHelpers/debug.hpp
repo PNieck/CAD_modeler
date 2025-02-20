6be6bd83e6297fb4839bd0575db95e03681e4157
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2025-02-20 23:17:16 +0100

    Refactor OpenGL debug module

---
+++ include/openglHelpers/debug.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <glad/glad.h>
+
+
+GLenum glCheckError_(const char *file,int line);
+
+
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
--- src/openglHelpers/debug.cpp
+++ src/openglHelpers/debug.cpp
@@ -1,10 +1,9 @@
-#pragma once
+#include <openglHelpers/debug.hpp>
 
-#include <glad/glad.h>
 #include <iostream>
 
 
-GLenum glCheckError_(const char *file, int line)
+GLenum glCheckError_(const char *file, const int line)
 {
     GLenum errorCode;
     while ((errorCode = glGetError()) != GL_NO_ERROR)
@@ -12,18 +11,38 @@ GLenum glCheckError_(const char *file, int line)
         std::string error;
         switch (errorCode)
         {
+#       ifdef GL_INVALID_ENUM
             case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+#       endif
+
+#       ifdef GL_INVALID_VALUE
             case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+#       endif
+
+#       ifdef GL_INVALID_OPERATION
             case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
-            //case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
-            //case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+#       endif
+
+#       ifdef GL_STACK_OVERFLOW
+            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+#       endif
+
+#       ifdef GL_STACK_UNDERFLOW
+            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+#       endif
+
+#       ifdef GL_OUT_OF_MEMORY
             case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+#       endif
+
+#       ifdef GL_INVALID_FRAMEBUFFER_OPERATION
             case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+#       endif
+
+            default: throw std::runtime_error("Unknown OpenGL error code");
         }
+
         std::cout << error << " | " << file << " (" << line << ")" << std::endl;
     }
     return errorCode;
 }
-
-
-#define glCheckError() glCheckError_(__FILE__, __LINE__)
