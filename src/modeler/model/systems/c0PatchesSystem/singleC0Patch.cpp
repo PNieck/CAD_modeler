551a811b5eca5de56c3aec4516a9031e24ed0e35
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2025-04-05 00:38:29 +0200

    FIx C0 patches calculations

--- src/modeler/model/systems/c0PatchesSystem/singleC0Patch.cpp
+++ src/modeler/model/systems/c0PatchesSystem/singleC0Patch.cpp
@@ -1,15 +1,17 @@
 #include <CAD_modeler/model/systems/c0PatchesSystem/singleC0Patch.hpp>
 
+#include "CAD_modeler/model/systems/c0PatchesSystem.hpp"
 
-SingleC0Patch::SingleC0Patch(Coordinator& coord, const C0Patches &patches, float u, float v):
+
+SingleC0Patch::SingleC0Patch(Coordinator& coord, const C0Patches &patches, const float u, const float v):
     coordinator(coord),
     patches(patches),
     firstRow(static_cast<int>(std::floor(v)) * 3),
     firstCol(static_cast<int>(std::floor(u)) * 3)
 {
-    if (v == static_cast<float>(patches.PatchesInRow()))
+    if (v == C0PatchesSystem::MaxV(patches))
         --firstRow;
 
-    if (u == static_cast<float>(patches.PatchesInCol()))
+    if (u == C0PatchesSystem::MaxU(patches))
         --firstCol;
 }
