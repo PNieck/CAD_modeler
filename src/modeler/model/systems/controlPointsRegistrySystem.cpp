bc209a99675b900c6afe8efe0637d358dbd6f0db
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2025-11-04 21:11:22 +0100

    Refactor controlPointsRegistrySystem implementation

--- src/modeler/model/systems/controlPointsRegistrySystem.cpp
+++ src/modeler/model/systems/controlPointsRegistrySystem.cpp
@@ -1,9 +1,10 @@
 #include <CAD_modeler/model/systems/controlPointsRegistrySystem.hpp>
 
 
-void ControlPointsRegistrySystem::UnregisterControlPoint(Entity owner, const Entity cp, SystemId system)
+void ControlPointsRegistrySystem::UnregisterControlPoint(const Entity owner, const Entity cp, const SystemId system)
 {
-    if (!controlPointsOwners.contains(cp))
+    const auto it = controlPointsOwners.find(cp);
+    if (it == controlPointsOwners.end())
         return;
 
     auto& ownersSet = controlPointsOwners.at(cp);
@@ -11,4 +12,9 @@ void ControlPointsRegistrySystem::UnregisterControlPoint(Entity owner, const Ent
 
     if (ownersSet.empty())
         controlPointsOwners.erase(cp);
+    auto& ownersSet = it->second;
+    if (ownersSet.erase({owner, system}) > 0)
+        if (ownersSet.empty())
+            controlPointsOwners.erase(it);
+}
 }
